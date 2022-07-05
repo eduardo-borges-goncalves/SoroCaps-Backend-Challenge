15127fcd0d7d1cbc9c3e72e9a438a04087187676
Eduardo Borges <92747098+eduardo-borges-goncalves@users.noreply.github.com>
2022-07-05 15:25:13 -0300

    start server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+
+// conect with database 
+
+// create routes
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
