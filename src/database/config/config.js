5a2fb894db3a74167924723b669e6fd587ab0377
Eduardo Borges <alemaobrk1@gmail.com>
2022-07-10 18:32:20 -0300

    change database setup

---
+++ src/database/config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+module.exports = {
+  
+  "local": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASS,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST, 
+    "dialect": process.env.DATABASE_DIALECT
+  },
+  "development": {  
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASS,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST, 
+    "dialect": process.env.DATABASE_DIALECT
+  },
+  "test": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASS,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST, 
+    "dialect": process.env.DATABASE_DIALECT
+  },
+  "production": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASS,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST, 
+    "dialect": process.env.DATABASE_DIALECT
+  }
+}
