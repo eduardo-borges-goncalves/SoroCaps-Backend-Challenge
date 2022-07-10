4bc3eb08a814cd0bf1313209b47168ac22dac8ed
Eduardo Borges <alemaobrk1@gmail.com>
2022-07-10 13:13:40 -0300

    implements jwt

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,2 +1,20 @@
-// npm i jsonwebtoken 
-// npm i @types/jsonwebtoken -D
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken"
+
+export const authentication = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token é obrigatório!" });
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    process.env.APP_SECRET && await jwt.verify(token, process.env.APP_SECRET)
+    next()
+
+  } catch (error: any) {
+    return res.status(401).json({ message: "Token inválido!" });
+  }
+}
+
