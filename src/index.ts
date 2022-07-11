0a129e75e0f9eb570b14bf1b643c895232af90c5
Eduardo Borges <alemaobrk1@gmail.com>
2022-07-11 09:05:07 -0300

    implement log system

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import "./config/env";
 import express, { json } from "express"
 import router from "./routes"
-
 import { db } from "./models";
 import cors from "cors";
 
+const morganBody = require('morgan-body')
+const fs = require('fs')
+const path = require('path')
+const moment = require('moment')
+
 const app = express()
+
+const log = fs.createWriteStream(
+  path.join(__dirname, "./logs", `${moment().format('YYYY-MM-DD')}.log`),
+  { flags: "a"}
+)
+morganBody(app, {
+  noColors: true, 
+  stream: log
+})
 
 // create swager       
 
